290053b5e625c91ea3a50a5cb85360005718fe57
softwareByAndi <softwareByAndi@gmail.com>
2024-03-01 15:38:50 -0700

    implement a queue for a uniformly random character pull

--- README.md
+++ README.md
@@ -1,4 +1,56 @@
 # typing-practice
 a js script for typing practice in the terminal for programmers who want just 5 quick minutes to improve reaction speed and typing speed. 
 
-Characters appear 1 at a time, so this script improves reading comprehension, reaction timing, and typing speed.
+Random characters appear 1 at a time, so this script improves reading comprehension, reaction timing, and typing speed.
+
+## interface looks like this
+
+``` text
+
+ Typing Practice - round 3
+ 11.7 : run avg wpm    1.02s : run avg time:
+  6.9 : current wpm    1.74s : current time: 
+
+ #
+
+```
+
+## logic notes
+
+- characters are sorted randomly into a queue, and popped off 1 at a time.
+    - every time the character queue is emptied, it's re-populated and re-shuffled.
+    - this is to ensure a random, but uniformly distributed character pull, so that probabilities for each charcter can be controlled via its number of occurrences in the set.
+- the `running avg wpm` & `running avg time` have a default window of 10 characters. (i.e. the average over the last 10 characters)
+- the `current wpm` & `current time` are calculated for the last character only.
+- the `round` is the current iteration of the character queue.
+    - the round is incremented every time the character queue is emptied and re-populated.
+
+## type Esc to skip a character
+
+sometimes the terminal will bug out and the character to type will dissapear. 
+It wasn't important enough to fix, so just type Esc to skip the character and move on to the next one.
+
+## aliasing
+
+I recommend adding an alias to your bashrc or zshrc to make it easier to run the script. 
+``` bash
+code ~/.bashrc
+```
+
+``` bash
+# ~/.bashrc or ~/.zshrc
+alias typing-practice="node ~/path/to/repository/typing-practice/typing_practice.js"
+```
+
+``` bash
+source ~/.bashrc
+```
+
+<br>
+
+# TODO
+
+1. maybe add some in sight logging to track performance over time.
+    - what about a scoreboard? Competing against past performances.
+    - maybe add some some average tracking across rounds.
+
---
+++ typing_practice.js
@@ -0,0 +1,163 @@
+const { ALL } = require('dns');
+const readline = require('readline');
+
+/**
+ * character_queue:
+ * a shuffled queue of characters to type; to ensure that all characters in the set show each round 
+ * When empty, shuffle again
+ */
+let character_queue = []
+let current_round = 0
+const g_inputTimestamps = []
+const g_timeDiffs = []
+
+const NUM_CHARS_PER_WORD = 5
+const RUNNING_AVG_WINDOW_SIZE = 10
+const MODULUS_FACTOR = 1000000 // 1_000_000
+
+const ALPHABET_LOWERCASE = 'abcdefghijklmnopqrstuvwxyz'
+const ALPHABET = ALPHABET_LOWERCASE + ALPHABET_LOWERCASE.toUpperCase()
+const DIGITS = '0123456789'
+const SPECIAL_CHARACTERS = '!@#$%^&*()_+[{}]:;<>,.?/~`-=|\\"\'`'
+const ALL_CHARACTERS = ALPHABET
+    + DIGITS
+    + DIGITS
+    + SPECIAL_CHARACTERS
+    + SPECIAL_CHARACTERS
+    + SPECIAL_CHARACTERS
+
+// Create readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Set stdin in raw mode to read characters individually
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+function exit() {
+    process.exit()
+}
+
+function print(str) {
+    process.stdout.write(str)
+}
+
+function clear_page() {
+    console.clear()
+}
+
+function clear_line() {
+    print('\x1b[2K\r ');
+}
+
+function clear_below_line(lineNumber) {
+    // Move the cursor to the beginning of the desired line
+    process.stdout.write(`\x1b[${lineNumber};1H`);
+    // Clear the screen from the cursor down
+    process.stdout.write('\x1b[J ');
+}
+
+function shuffle() {
+    character_queue = [...ALL_CHARACTERS]
+        .sort(() => Math.random() - 0.5)
+}
+
+function update_time_diffs() {
+    const currentTime = Date.now();
+    if (g_inputTimestamps.length > 0) {
+        const prevTime = g_inputTimestamps[g_inputTimestamps.length - 1]
+        g_timeDiffs.push(currentTime - prevTime)
+    }
+    g_inputTimestamps.push(currentTime)
+}
+
+function print_header() {
+    const MS_IN_SEC = 1000
+    const SEC_IN_MIN = 60
+    let avg_time_s = 0
+    let current_time_s = 0
+    let avg_wpm = 0
+    let current_wpm = 0
+    const diff_length = g_timeDiffs.length
+    if (diff_length) {
+        const window_start = Math.max(0, diff_length - RUNNING_AVG_WINDOW_SIZE)
+        const avg_time_ms = g_timeDiffs.slice(window_start).reduce((a, b) => a + b) / (diff_length - window_start)
+        avg_time_s = avg_time_ms / MS_IN_SEC
+        current_time_s = g_timeDiffs[g_timeDiffs.length - 1] / MS_IN_SEC
+        avg_wpm = SEC_IN_MIN / (avg_time_s * NUM_CHARS_PER_WORD)
+        current_wpm = SEC_IN_MIN / (current_time_s * NUM_CHARS_PER_WORD)
+    }
+    print(`\n Typing Practice - round ${current_round}\n`)
+    print(` ${avg_wpm.toFixed(1).padStart(4, ' ')    } : run avg wpm    ${avg_time_s.toFixed(2)    }s : run avg time:\n`)
+    print(` ${current_wpm.toFixed(1).padStart(4, ' ')} : current wpm    ${current_time_s.toFixed(2)}s : current time: \n`)
+}
+
+function readKey() {
+    return new Promise(resolve => {
+        process.stdin.once('data', function (key) {
+            // Exit on Ctrl+C
+            if (key === '\u0003') {
+                exit();
+            }
+            // Convert special keys to understandable representations or ignore them
+            if (key === '\r' || key === '\n') { // Enter key
+                clear_below_line(7)
+                key = 'Enter';
+            } else if (key === '\u001b') {
+                key = 'Escape';
+            } else if (key === ' ') { // Space
+                key = 'Space';
+            } else if (key === '\x7f' || key === '\b') { // Backspace (might vary based on system)
+                // Clear the line and return the cursor to the beginning of the line
+                clear_below_line(7)
+                key = 'Backspace';
+            }
+            resolve({
+                key
+            })
+        });
+    });
+}
+
+async function read_next_char(char=null) {
+    // print a random character from the alphabet
+    if (char === null) {
+        if (character_queue.length === 0) {
+            current_round++
+            shuffle()
+        }
+        char = character_queue.pop()
+        print_header()
+        print(`\n ${char}\n `)
+    }
+    const userInput = await readKey()
+    // get user character input from
+    return {
+        char,
+        userInput,
+        match: userInput.key === char,
+        skip: userInput.key === 'Escape',
+        time: userInput.time
+    }
+}
+
+async function main() {
+    clear_page()
+    let input = await read_next_char()
+    while (true) {
+        while (!input.match && !input.skip) {
+            input = await read_next_char(input.char)
+        }
+        clear_page()
+        if (input.match) {
+            update_time_diffs()
+        }
+        input = await read_next_char()
+    }
+    exit()
+}
+
+main()
